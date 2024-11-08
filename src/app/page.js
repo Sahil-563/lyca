6c23e3202869c2f585734d52dfa509e59b433c0e
Sahil-563 <sahil2002ece@gmail.com>
2024-11-08 10:57:47 +0530

    added whyLyca component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import BannerTwo from "./components/BannerTwo/BannerTwo";
 import Card from "./components/card/Card";
 import Download from "./components/download/Download";
 import BannerOne from "./components/Slider/BannerOne";
-import WhyLyca from "./components/whyLyca/whyLyca";
+import WhyLyca from "./components/whyLyca/WhyLyca";
 
 export default function Home() {
   return (
